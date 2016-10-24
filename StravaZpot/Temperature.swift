b88792d921fcfb9a804ffddde8a09a2fb590668c
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:24:08 +0200

    Create struct to represent Temperature

---
+++ StravaZpot/Temperature.swift
@@ -0,0 +1,13 @@
+//
+//  Temperature.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Temperature {
+    public let celsiusDegrees : Double
+}
