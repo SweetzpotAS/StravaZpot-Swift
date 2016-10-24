6f0cd1daaf9d1cf88eb38591a7a75b470bb89e42
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:22:09 +0200

    Create struct to represent speed

---
+++ StravaZpot/Speed.swift
@@ -0,0 +1,13 @@
+//
+//  Speed.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Speed {
+    public let metersPerSecond : Double
+}
