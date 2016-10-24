301cbe65bcc0ed167b6f15acb7363c2ae8235677
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 15:31:33 +0200

    Initial Commit

---
+++ StravaZpot/StravaZpot.h
@@ -0,0 +1,19 @@
+//
+//  StravaZpot.h
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StravaZpot.
+FOUNDATION_EXPORT double StravaZpotVersionNumber;
+
+//! Project version string for StravaZpot.
+FOUNDATION_EXPORT const unsigned char StravaZpotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StravaZpot/PublicHeader.h>
+
+
