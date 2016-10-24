19c5a500a8f721fe817f086dd20e65c55b4583cb
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:49:59 +0200

    Create struct to represent map

---
+++ StravaZpot/Map.swift
@@ -0,0 +1,15 @@
+//
+//  Map.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Map {
+    public let id : Int
+    public let resourceState : ResourceState
+    public let summaryPolyline : String
+}
