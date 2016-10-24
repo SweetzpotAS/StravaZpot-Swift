24e1857f970a04da82b6be2d25be1c88c135b76e
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:19:19 +0200

    Create struct to represent percentage

---
+++ StravaZpot/Percentage.swift
@@ -0,0 +1,13 @@
+//
+//  Percentage.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Percentage {
+    public let percent : Double
+}
