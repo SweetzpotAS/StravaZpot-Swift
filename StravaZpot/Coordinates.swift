7af0c784f0e79e3965c22027b5cda55b8169ceca
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:11:37 +0200

    Create struct to represent coordinates

---
+++ StravaZpot/Coordinates.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinates.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Coordinates {
+    public let latitude : Double
+    public let longitude : Double
+}
