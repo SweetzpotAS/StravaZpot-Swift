79a50992c0cad22950a59df5f404f9ea0b70c912
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:30:35 +0200

    Make timed interval implement equatable protocol

--- StravaZpot/TimedInterval.swift
+++ StravaZpot/TimedInterval.swift
@@ -13,3 +13,11 @@ public struct TimedInterval<T : Equatable> {
     public let max : T
     public let time : Int
 }
+
+extension TimedInterval : Equatable {}
+
+public func ==<T>(lhs : TimedInterval<T>, rhs : TimedInterval<T>) -> Bool where T : Equatable {
+    return lhs.min  == rhs.min  &&
+           lhs.max  == rhs.max  &&
+           lhs.time == rhs.time
+}
