970733417b2493ee5516e40a119238b068a697d8
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:14:04 +0200

    Create enum to represent gender

---
+++ StravaZpot/Gender.swift
@@ -0,0 +1,13 @@
+//
+//  Gender.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum Gender : String {
+    case male = "M", female = "F", notDefined = "null"
+}
