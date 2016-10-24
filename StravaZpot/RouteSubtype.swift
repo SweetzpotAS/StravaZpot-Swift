5815ea848bda4af22b3f69e8b461cfe671e6d7c5
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-24 16:53:47 +0200

    Create enum to represent route subtype

---
+++ StravaZpot/RouteSubtype.swift
@@ -0,0 +1,13 @@
+//
+//  RouteSubtype.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum RouteSubtype : Int {
+    case road = 1, mtb, cx, trail, mixed
+}
