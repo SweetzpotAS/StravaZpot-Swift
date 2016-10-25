9ade516d2072d619926602bcac0011e05b6f3143
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 11:44:44 +0200

    Create struct to represent totals

---
+++ StravaZpot/Totals.swift
@@ -0,0 +1,18 @@
+//
+//  Totals.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Totals {
+    public let count : Int
+    public let distance : Distance
+    public let movingTime : Time
+    public let elapsedTime : Time
+    public let elevationGain : Distance
+    public let achievementCount : Int
+}
