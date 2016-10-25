41e2e4dd517832f60daaf6c2773a86ce4701abec
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 11:01:06 +0200

    Make leaderboard implement equatable protocol

--- StravaZpot/Leaderboard.swift
+++ StravaZpot/Leaderboard.swift
@@ -12,3 +12,10 @@ public struct Leaderboard {
     public let entryCount : Int
     public let entries : EquatableArray<LeaderboardEntry>
 }
+
+extension Leaderboard : Equatable {}
+
+public func ==(lhs : Leaderboard, rhs : Leaderboard) -> Bool {
+    return  lhs.entryCount == rhs.entryCount &&
+            lhs.entries    == rhs.entries
+}
