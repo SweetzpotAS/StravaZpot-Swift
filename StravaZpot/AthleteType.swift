d2d1af0e71adf50f9df72d267fb1602eb1083fe0
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 11:32:30 +0200

    Create enum to represent athlete type

---
+++ StravaZpot/AthleteType.swift
@@ -0,0 +1,13 @@
+//
+//  AthleteType.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum AthleteType : Int {
+    case cyclist = 0, runner = 1
+}
