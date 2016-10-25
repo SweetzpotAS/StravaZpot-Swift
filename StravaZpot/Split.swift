5abb08af612b31285b92dd4f3aecc8c83c7d913c
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 14:13:43 +0200

    Create struct to represent split

---
+++ StravaZpot/Split.swift
@@ -0,0 +1,17 @@
+//
+//  Split.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Split {
+    public let distance : Distance
+    public let elevationDifferende : Distance
+    public let elapsedTime : Time
+    public let movingTime : Time
+    public let split : Int
+}
