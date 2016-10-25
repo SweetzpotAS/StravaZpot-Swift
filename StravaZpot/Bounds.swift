66a056b077b42662e148e11afb9c698ac9517e1b
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 10:31:25 +0200

    Make bounds implement equatable protocol

--- StravaZpot/Bounds.swift
+++ StravaZpot/Bounds.swift
@@ -12,3 +12,10 @@ public struct Bounds {
     public let southWest : Coordinates
     public let northEast : Coordinates
 }
+
+extension Bounds : Equatable {}
+
+public func ==(lhs : Bounds, rhs : Bounds) -> Bool {
+    return  lhs.southWest == rhs.southWest &&
+            lhs.northEast == rhs.northEast
+}
