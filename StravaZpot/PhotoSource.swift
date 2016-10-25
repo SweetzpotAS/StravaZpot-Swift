79f8e153f5a708095ef0cc234f4fb3e0b48a4838
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 13:54:40 +0200

    Create enum to represent photo source

---
+++ StravaZpot/PhotoSource.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoSource.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum PhotoSource : Int {
+    case strava = 1, instagram = 2
+}
