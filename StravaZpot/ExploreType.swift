d898063c6d1772cd98bab4359ba5f2be40502fa0
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 10:37:48 +0200

    Create enum to represent explore type

---
+++ StravaZpot/ExploreType.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreType.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum ExploreType : String {
+    case running = "running", riding = "riding"
+}
