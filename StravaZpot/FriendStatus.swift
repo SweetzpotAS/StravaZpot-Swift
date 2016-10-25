010be93a325e7a6f3622215c9e1d586f126e4bca
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 11:34:23 +0200

    Create enum to represent friend status

---
+++ StravaZpot/FriendStatus.swift
@@ -0,0 +1,16 @@
+//
+//  FriendStatus.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum FriendStatus : String {
+    case pending     = "pending",
+         accepted    = "accepted",
+         blocked     = "blocked",
+         not_friends = "null"
+}
