764b94372c79c52761ee8e421dc94d4a7f448830
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 14:07:15 +0200

    Make primary photo implement equatable protocol

--- StravaZpot/PrimaryPhoto.swift
+++ StravaZpot/PrimaryPhoto.swift
@@ -14,3 +14,12 @@ public struct PrimaryPhoto {
     public let source : PhotoSource
     public let urls : [String : String]
 }
+
+extension PrimaryPhoto : Equatable {}
+
+public func ==(lhs : PrimaryPhoto, rhs : PrimaryPhoto) -> Bool {
+    return  lhs.id       == rhs.id &&
+            lhs.uniqueID == rhs.uniqueID &&
+            lhs.source   == rhs.source &&
+            lhs.urls     == rhs.urls
+}
