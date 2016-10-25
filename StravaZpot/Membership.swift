13943d3cefb0ee734719343587efb21db7a8437a
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 12:46:35 +0200

    Create enum to represent membership

---
+++ StravaZpot/Membership.swift
@@ -0,0 +1,15 @@
+//
+//  Membership.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum Membership : String {
+    case member    = "member",
+         pending   = "pending",
+         notMember = "null"
+}
