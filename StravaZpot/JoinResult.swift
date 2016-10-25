69d77d6f2928f5576d18f23325e2792d02d3fd46
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 13:06:34 +0200

    Create class to represent join result

---
+++ StravaZpot/JoinResult.swift
@@ -0,0 +1,15 @@
+//
+//  JoinResult.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct JoinResult {
+    public let success : Bool
+    public let active : Bool
+    public let membership : Membership
+}
