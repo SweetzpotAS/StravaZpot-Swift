9a2c173527d4fc63c5a49cd970460062faf7910d
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 13:26:04 +0200

    Create enum to represent activity type

---
+++ StravaZpot/ActivityType.swift
@@ -0,0 +1,41 @@
+//
+//  ActivityType.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum ActivityType : String {
+    case ride            = "Ride",
+         run             = "Run",
+         swim            = "Swim",
+         hike            = "Hike",
+         walk            = "Walk",
+         alpineSki       = "AlpineSki",
+         backcountrySki  = "BackcountrySki",
+         canoeing        = "Canoeing",
+         crossfit        = "Crossfit",
+         ebikeRide       = "EBikeRide",
+         elliptical      = "Elliptical",
+         iceSkate        = "IceSkate",
+         inlineSkate     = "InlineSkate",
+         kayaking        = "Kayaking",
+         kiteSurf        = "KiteSurf",
+         nordicSki       = "NordicSki",
+         rockClimbing    = "RockClimbing",
+         rollerSki       = "RollerSki",
+         rowing          = "Rowing",
+         snowboard       = "Snowboard",
+         snowshoe        = "Snowshoe",
+         stairStepper    = "StairStepper",
+         standUpPaddling = "StandUpPaddling",
+         surfing         = "Surfing",
+         virtualRide     = "VirtualRide",
+         weightTraining  = "WeightTraining",
+         windsurf        = "Windsurf",
+         workout         = "Workout",
+         yoga            = "Yoga"
+}
