ea3b731af756a4f03e275d3c464df158acfd0432
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 11:23:50 +0200

    Create enum to represent resolution

---
+++ StravaZpot/Resolution.swift
@@ -0,0 +1,15 @@
+//
+//  Resolution.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum Resolution : String {
+    case low    = "low",
+         medium = "medium",
+         high   = "high"
+}
