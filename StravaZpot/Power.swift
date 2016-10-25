a5187a28ee51716583155b198d10ba956e484f1a
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 11:40:54 +0200

    Create struct to represent power

---
+++ StravaZpot/Power.swift
@@ -0,0 +1,13 @@
+//
+//  Power.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Power {
+    public let zones : EquatableArray<Interval<Double>>
+}
