684f33e0b413ae8aa4d91b68cb891e1b0c346607
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 12:00:49 +0200

    Create struct ro represent zones

---
+++ StravaZpot/Zones.swift
@@ -0,0 +1,14 @@
+//
+//  Zones.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct Zones {
+    public let heartRate : HeartRate
+    public let power : Power
+}
