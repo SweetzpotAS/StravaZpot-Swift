ea9c7d6da4eea52b62e780f978aade0810ae8e6e
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 14:19:28 +0200

    Create enum to represent workout type

---
+++ StravaZpot/WorkoutType.swift
@@ -0,0 +1,19 @@
+//
+//  WorkoutType.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum WorkoutType : Int {
+    case defaultRun  = 0,
+         raceRun     = 1,
+         longRun     = 2,
+         workoutRun  = 3,
+         defaultRide = 10,
+         raceRide    = 11,
+         workoutRide = 12
+}
