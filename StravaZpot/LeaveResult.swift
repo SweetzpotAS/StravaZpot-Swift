a7495a2dd122062baaa2c970d4226d2da67d4667
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 13:10:08 +0200

    Create struct to represent leave result

---
+++ StravaZpot/LeaveResult.swift
@@ -0,0 +1,14 @@
+//
+//  LeaveResult.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct LeaveResult {
+    public let success : Bool
+    public let active : Bool
+}
