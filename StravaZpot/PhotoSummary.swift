cf025be8cc00971bce199bac8e9291d849bc432f
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 14:09:29 +0200

    Create struct to represent photo summary

---
+++ StravaZpot/PhotoSummary.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoSummary.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct PhotoSummary {
+    public let count : Int
+    public let primary : PrimaryPhoto
+}
