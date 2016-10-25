d6f91c9294e501f1f3e79690059e02475a1e00f4
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 10:43:03 +0200

    Create enum to represent weight class

---
+++ StravaZpot/WeightClass.swift
@@ -0,0 +1,24 @@
+//
+//  WeightClass.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum WeightClass : String {
+    case pounds_0_124    = "0_124",
+         pounds_125_149  = "125_149",
+         pounds_150_164  = "150_164",
+         pounds_165_179  = "165_179",
+         pounds_180_199  = "180_199",
+         pounds_200_plus = "200_plus",
+         kg_0_54         = "0_54",
+         kg_55_64        = "55_64",
+         kg_65_74        = "65_74",
+         kg_75_84        = "75_84",
+         kg_85_94        = "85_94",
+         kg_95_plus      = "95_plus"
+}
