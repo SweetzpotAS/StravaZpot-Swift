4fbaae0253972afb6b9e73a10864ece44e0db5f4
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-25 11:25:16 +0200

    Create enum to represent series type

---
+++ StravaZpot/SeriesType.swift
@@ -0,0 +1,14 @@
+//
+//  SeriesType.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 25/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public enum SeriesType : String {
+    case time     = "time",
+         distance = "distance"
+}
