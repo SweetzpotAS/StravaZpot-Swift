ece5c1dc28a389927b4e84a2c5378f0564aedc63
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-26 10:46:05 +0200

    Make activity lap implement equatable protocol

--- StravaZpot/ActivityLap.swift
+++ StravaZpot/ActivityLap.swift
@@ -32,3 +32,30 @@ public struct ActivityLap {
     public let averageHeartRate : Double
     public let lapIndex : Int
 }
+
+extension ActivityLap : Equatable {}
+
+public func ==(lhs : ActivityLap, rhs : ActivityLap) -> Bool {
+    return  lhs.id                 == rhs.id &&
+            lhs.resourceState      == rhs.resourceState &&
+            lhs.name               == rhs.name &&
+            lhs.activity           == rhs.activity &&
+            lhs.athlete            == rhs.athlete &&
+            lhs.elapsedTime        == rhs.elapsedTime &&
+            lhs.movingTime         == rhs.movingTime &&
+            lhs.startDate          == rhs.startDate &&
+            lhs.startDateLocal     == rhs.startDateLocal &&
+            lhs.distance           == rhs.distance &&
+            lhs.startIndex         == rhs.startIndex &&
+            lhs.endIndex           == rhs.endIndex &&
+            lhs.totalElevationGain == rhs.totalElevationGain &&
+            lhs.averageSpeed       == rhs.averageSpeed &&
+            lhs.maxSpeed           == rhs.maxSpeed &&
+            lhs.averageCadence     == rhs.averageCadence &&
+            lhs.averageWatts       == rhs.averageWatts &&
+            lhs.hasDeviceWatts     == rhs.hasDeviceWatts &&
+            lhs.hasHeartRate       == rhs.hasHeartRate &&
+            lhs.maxHeartRate       == rhs.maxHeartRate &&
+            lhs.averageHeartRate   == rhs.averageHeartRate &&
+            lhs.lapIndex           == rhs.lapIndex
+}
