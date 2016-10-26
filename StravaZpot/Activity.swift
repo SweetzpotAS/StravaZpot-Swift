08220a533feeff909611e661a553a60a8b406f96
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-26 10:33:41 +0200

    Make activity implement equatable protocol

--- StravaZpot/Activity.swift
+++ StravaZpot/Activity.swift
@@ -64,3 +64,62 @@ public struct Activity {
     public let splitsMetric : EquatableArray<Split>?
     public let splitsStandard : EquatableArray<Split>?
 }
+
+extension Activity : Equatable {}
+
+public func ==(lhs : Activity, rhs : Activity) -> Bool {
+    return  lhs.id == rhs.id &&
+            lhs.resourceState        == rhs.resourceState &&
+            lhs.externalID           == rhs.externalID &&
+            lhs.uploadID             == rhs.uploadID &&
+            lhs.athlete              == rhs.athlete &&
+            lhs.name                 == rhs.name &&
+            lhs.description          == rhs.description &&
+            lhs.distance             == rhs.distance &&
+            lhs.movingTime           == rhs.movingTime &&
+            lhs.elapsedTime          == rhs.elapsedTime &&
+            lhs.totalElevationGain   == rhs.totalElevationGain &&
+            lhs.elevationHigh        == rhs.elevationHigh &&
+            lhs.elevationLow         == rhs.elevationLow &&
+            lhs.type                 == rhs.type &&
+            lhs.startDate            == rhs.startDate &&
+            lhs.startDateLocal       == rhs.startDateLocal &&
+            lhs.timezone             == rhs.timezone &&
+            lhs.startCoordinates     == rhs.startCoordinates &&
+            lhs.endCoordinates       == rhs.endCoordinates &&
+            lhs.achievementCount     == rhs.achievementCount &&
+            lhs.kudosCount           == rhs.kudosCount &&
+            lhs.commentCount         == rhs.commentCount &&
+            lhs.athleteCount         == rhs.athleteCount &&
+            lhs.photoCount           == rhs.photoCount &&
+            lhs.totalPhotoCount      == rhs.totalPhotoCount &&
+            lhs.photos               == rhs.photos &&
+            lhs.map                  == rhs.map &&
+            lhs.hasTrainer           == rhs.hasTrainer &&
+            lhs.isCommute            == rhs.isCommute &&
+            lhs.isManual             == rhs.isManual &&
+            lhs.isPrivate            == rhs.isPrivate &&
+            lhs.deviceName           == rhs.deviceName &&
+            lhs.embedToken           == rhs.embedToken &&
+            lhs.isFlagged            == rhs.isFlagged &&
+            lhs.workoutType          == rhs.workoutType &&
+            lhs.gearID               == rhs.gearID &&
+            lhs.gear                 == rhs.gear &&
+            lhs.averageSpeed         == rhs.averageSpeed &&
+            lhs.maxSpeed             == rhs.maxSpeed &&
+            lhs.averageCadence       == rhs.averageCadence &&
+            lhs.averageTemperature   == rhs.averageTemperature &&
+            lhs.averageWatts         == rhs.averageWatts &&
+            lhs.maxWatts             == rhs.maxWatts &&
+            lhs.weightedAverageWatts == rhs.weightedAverageWatts &&
+            lhs.kilojoules           == rhs.kilojoules &&
+            lhs.hasDeviceWatts       == rhs.hasDeviceWatts &&
+            lhs.hasHeartRate         == rhs.hasHeartRate &&
+            lhs.averageHeartRate     == rhs.averageHeartRate &&
+            lhs.maxHeartRate         == rhs.maxHeartRate &&
+            lhs.calories             == rhs.calories &&
+            lhs.sufferScore          == rhs.sufferScore &&
+            lhs.hasKudoed            == rhs.hasKudoed &&
+            lhs.splitsMetric         == rhs.splitsMetric &&
+            lhs.splitsStandard       == rhs.splitsStandard
+}
