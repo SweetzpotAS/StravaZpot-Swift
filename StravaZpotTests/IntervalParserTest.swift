ad6286a11a219dac97e89fb4510c5b27273b7239
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-28 12:30:35 +0200

    Create interval parser

---
+++ StravaZpotTests/IntervalParserTest.swift
@@ -0,0 +1,25 @@
+//
+//  IntervalParserTest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 28/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import XCTest
+import SwiftyJSON
+import Nimble
+@testable import StravaZpot
+
+class IntervalParserTest: XCTestCase {
+    
+    func testJSONIsParsedToIntervalOfDouble() {
+        let json = JSON.parse("{\"min\" : 1.0, \"max\" : 7.5}")
+        let parser = IntervalDoubleParser()
+        
+        let result = parser.from(json : json)
+        let expected = Interval<Double>(min: 1.0, max: 7.5)
+        
+        expect(result).to(equal(expected))
+    }
+}
