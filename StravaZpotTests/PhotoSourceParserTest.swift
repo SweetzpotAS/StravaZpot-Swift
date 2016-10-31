7d33824c4912eed651ee485dc63b353efa59d75f
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 10:05:33 +0100

    Create photo source parser

---
+++ StravaZpotTests/PhotoSourceParserTest.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoSourceParserTest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import XCTest
+import SwiftyJSON
+import Nimble
+@testable import StravaZpot
+
+class PhotoSourceParserTest: XCTestCase {
+    
+    func testJSONisParsedToPhotoSource() {
+        let json = JSON.parse("1")
+        let parser = PhotoSourceParser()
+        
+        let result = parser.from(json: json)
+        let expected = PhotoSource.strava
+        
+        expect(result).to(equal(expected))
+    }
+    
+}
