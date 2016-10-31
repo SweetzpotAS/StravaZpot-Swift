3f6598a5c0c2686c378de4e7589aaae2b6e9a25e
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 13:21:57 +0100

    Create route subtype parser

---
+++ StravaZpotTests/RouteSubtypeParserTest.swift
@@ -0,0 +1,25 @@
+//
+//  RouteSubtypeParserTest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import SwiftyJSON
+@testable import StravaZpot
+
+class RouteSubtypeParserTest : XCTestCase {
+    
+    func testJSONisParsedToRouteSubtype() {
+        let json = JSON.parse("3")
+        let parser = RouteSubtypeParser()
+        
+        let result = parser.from(json: json)
+        let expected = RouteSubtype.cx
+        
+        expect(result).to(equal(expected))
+    }
+}
