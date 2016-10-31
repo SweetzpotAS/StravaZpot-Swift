968876e9ac5e4690615a13bb9ec902666b702103
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 11:21:06 +0100

    Create measurement preference parser

---
+++ StravaZpotTests/MeasurementPreferenceParserTest.swift
@@ -0,0 +1,26 @@
+//
+//  MeasurementPreferenceParserTest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import SwiftyJSON
+@testable import StravaZpot
+
+class MeasurementPreferenceParserTest: XCTestCase {
+    
+    func testJSONisParsedToMeasurementPreference() {
+        let json = JSON.parse("\"feet\"")
+        let parser = MeasurementPreferenceParser()
+        
+        let result = parser.from(json: json)
+        let expected = MeasurementPreference.feet
+        
+        expect(result).to(equal(expected))
+    }
+    
+}
