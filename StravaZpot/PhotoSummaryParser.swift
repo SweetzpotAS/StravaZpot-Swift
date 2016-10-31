11f727d04aff9f2faabb3ab3abcaea31ddedf3e8
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 11:08:40 +0100

    Create photo summary parser

---
+++ StravaZpot/PhotoSummaryParser.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSummaryParser.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class PhotoSummaryParser : Parser {
+    public func from(json: JSON) -> PhotoSummary {
+        return PhotoSummary(count : json["count"].int!,
+                            primary : PrimaryPhotoParser().from(json: json["primary"]))
+    }
+}
