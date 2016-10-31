797259973fc716d3f0636323d7502207ecc2b297
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 16:51:42 +0100

    Create stream parser

---
+++ StravaZpot/StreamParser.swift
@@ -0,0 +1,20 @@
+//
+//  StreamParser.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class StreamParser : Parser {
+    public func from(json: JSON) -> Stream {
+        return Stream(type: StreamTypeParser().from(json: json["type"]),
+                      data: json["data"].array!,
+                      seriesType: SeriesTypeParser().from(json: json["series_type"]),
+                      originalSize: json["original_size"].int!,
+                      resolution: ResolutionParser().from(json: json["resolution"]))
+    }
+}
