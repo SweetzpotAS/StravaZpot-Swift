3ad8748d2a8fc66df42bfdeb5cd2cc9b0b334411
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 12:56:51 +0100

    Create join result parser

---
+++ StravaZpot/JoinResultParser.swift
@@ -0,0 +1,18 @@
+//
+//  JoinResultParser.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class JoinResultParser : Parser {
+    public func from(json: JSON) -> JoinResult {
+        return JoinResult(success: json["success"].bool!,
+                          active: json["active"].bool!,
+                          membership: MembershipParser().from(json: json["membership"]))
+    }
+}
---
+++ StravaZpotTests/JoinResultParserTest.swift
@@ -0,0 +1,26 @@
+//
+//  JoinResultParserTest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import SwiftyJSON
+@testable import StravaZpot
+
+class JoinResultParserTest: XCTestCase {
+    
+    func testJSONisParsedToJoinResult() {
+        let json = JSON.parse("{\"success\": true, \"active\": true, \"membership\": \"pending\"}")
+        let parser = JoinResultParser()
+        
+        let result = parser.from(json: json)
+        let expected = JoinResult(success: true, active: true, membership: .pending)
+        
+        expect(result).to(equal(expected))
+    }
+    
+}
