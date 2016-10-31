2f3025a38c80c6603dfc7edcf64fdf5fb85dfa8b
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 13:43:11 +0100

    Create series type parser

---
+++ StravaZpotTests/SeriesTypeParserTest.swift
@@ -0,0 +1,26 @@
+//
+//  SeriesTypeParserTest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import SwiftyJSON
+@testable import StravaZpot
+
+class SeriesTypeParserTest: XCTestCase {
+    
+    func testJSONisParsedToSeriesType() {
+        let json = JSON.parse("\"time\"")
+        let parser = SeriesTypeParser()
+        
+        let result = parser.from(json: json)
+        let expected = SeriesType.time
+        
+        expect(result).to(equal(expected))
+    }
+    
+}
