817bf241308745d2b3d9fb8029450d6c0951a219
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-10-31 10:10:30 +0100

    Create workout type parser

---
+++ StravaZpot/WorkoutTypeParser.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutTypeParser.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class WorkoutTypeParser : Parser {
+    public func from(json: JSON) -> WorkoutType {
+        return WorkoutType(rawValue: json.int!)!
+    }
+}
---
+++ StravaZpotTests/WorkoutTypeParserTest.swift
@@ -0,0 +1,25 @@
+//
+//  WorkoutTypeParserTest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 31/10/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import XCTest
+import SwiftyJSON
+import Nimble
+@testable import StravaZpot
+
+class WorkoutTypeParserTest: XCTestCase {
+    
+    func testJSONisParsedToWorkoutType() {
+        let json = JSON.parse("11")
+        let parser = WorkoutTypeParser()
+        
+        let result = parser.from(json: json)
+        let expected = WorkoutType.raceRide
+        
+        expect(result).to(equal(expected))
+    }
+}
