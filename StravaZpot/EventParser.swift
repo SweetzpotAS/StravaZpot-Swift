10d343d480eada87e078472e3053e5bcaeb21def
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-11-01 12:03:58 +0100

    Create event parser

---
+++ StravaZpot/EventParser.swift
@@ -0,0 +1,35 @@
+//
+//  EventParser.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 1/11/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class EventParser : Parser {
+    public func from(json: JSON) -> Event {
+        return Event(id: json["id"].int!,
+                     resourceState: ResourceStateParser().from(json: json["resource_state"]),
+                     title: json["title"].string!,
+                     description: json["description"].string!,
+                     clubID: json["club_id"].int!,
+                     organizingAthlete: AthleteParser().from(json: json["organizing_athlete"]),
+                     activityType: ActivityTypeParser().from(json: json["activity_type"]),
+                     createdAt: DateParser().from(json: json["created_at"]),
+                     routeID: json["route_id"].int!,
+                     isWomanOnly: json["women_only"].bool!,
+                     isPrivate: json["private"].bool!,
+                     skillLevel: SkillLevelParser().from(json: json["skill_levels"]),
+                     terrain: TerrainParser().from(json: json["terrain"]),
+                     upcomingOccurrences: getDates(json: json["upcoming_occurrences"]),
+                     address: json["address"].string!)
+    }
+    
+    private func getDates(json : JSON) -> EquatableArray<Date> {
+        let dateParser = DateParser()
+        return EquatableArray<Date>(array: json.map { dateParser.from(json: $1) })
+    }
+}
