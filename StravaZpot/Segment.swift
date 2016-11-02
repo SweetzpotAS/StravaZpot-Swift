9e9501779fdc8a04aee152d3f0fa31af5dc5fad2
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-11-02 09:35:08 +0100

    Change optional fields that may not be filled from the API

--- StravaZpot/Segment.swift
+++ StravaZpot/Segment.swift
@@ -12,21 +12,21 @@ public struct Segment {
     public let id : Int
     public let resourceState : ResourceState
     public let name : String
-    public let activityType : ActivityType
-    public let distance : Distance
-    public let averageGrade : Percentage
-    public let maximumGrade : Percentage
-    public let elevationHigh : Distance
-    public let elevationLow : Distance
-    public let startCoordinates : Coordinates
-    public let endCoordinates : Coordinates
-    public let climbCategory : Int
-    public let city : String
-    public let state : String
-    public let country : String
-    public let isPrivate : Bool
-    public let isStarred : Bool
-    public let isHazardous : Bool
+    public let activityType : ActivityType?
+    public let distance : Distance?
+    public let averageGrade : Percentage?
+    public let maximumGrade : Percentage?
+    public let elevationHigh : Distance?
+    public let elevationLow : Distance?
+    public let startCoordinates : Coordinates?
+    public let endCoordinates : Coordinates?
+    public let climbCategory : Int?
+    public let city : String?
+    public let state : String?
+    public let country : String?
+    public let isPrivate : Bool?
+    public let isStarred : Bool?
+    public let isHazardous : Bool?
     public let createdAt : Date?
     public let updatedAt : Date?
     public let totalElevationGain : Distance?
