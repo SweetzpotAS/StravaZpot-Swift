fc0942ac3a3dd7ee47d82dd8f75692d206a8524c
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-11-03 10:30:13 +0100

    Create protocol for request accepting pagination

---
+++ StravaZpot/PaginatedRequest.swift
@@ -0,0 +1,27 @@
+//
+//  PaginatedRequest.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 3/11/16.
+//  Copyright © 2016 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+protocol PaginatedRequest : class {
+    var page : Int? { get set }
+    var perPage : Int? { get set }
+}
+
+extension PaginatedRequest {
+    func of(page: Int, itemsPerPage : Int) -> Self {
+        self.page = page
+        self.perPage = itemsPerPage
+        return self
+    }
+    
+    func pageParameters() -> [String : Any] {
+        return [ "page" : self.page,
+                 "per_page" : self.perPage ].flatten()
+    }
+}
