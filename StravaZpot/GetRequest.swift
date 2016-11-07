c3a5c8ed8e70c2278a6138024015f77c5d900fa1
Tomás Ruiz-López <trl.ironside@gmail.com>
2016-11-07 10:30:53 +0100

    Extract superclass to remove duplication

--- StravaZpot/GetRequest.swift
+++ StravaZpot/GetRequest.swift
@@ -9,33 +9,8 @@
 import Foundation
 import SwiftyJSON
 
-public class GetRequest<T : Equatable> {
-    private let client : HTTPClient
-    private let url : String
-    private let parse : (JSON) -> T?
-    
-    init(client : HTTPClient, url : String, parse : @escaping (JSON) -> T?) {
-        self.client = client
-        self.url = url
-        self.parse = parse
-    }
-    
-    public func execute(callback : @escaping (StravaResult<T>) -> ()) {
-        client.get(url: url, parameters: getParameters().flatten()){ result in
-            switch(result) {
-            case let .success(json):
-                if let parsed = self.parse(json) {
-                    callback(.success(parsed))
-                } else {
-                    callback(.error(StravaError.apiError(message: "Error parsing \(json)")))
-                }
-            case let .error(content):
-                callback(.error(content))
-            }
-        }
-    }
-    
-    func getParameters() -> [String : Any] {
-        return [:]
+public class GetRequest<T : Equatable> : Request<T>{
+    override public func execute(callback : @escaping (StravaResult<T>) -> ()) {
+        client.get(url: url, parameters: getParameters().flatten(), callback: processResult(callback: callback))
     }
 }
