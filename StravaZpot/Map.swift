b040af32be1bf06db54d46be4bdf1bffa6d11fab
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-07-24 11:50:04 +0200

    Add polyline to Map

--- StravaZpot/Map.swift
+++ StravaZpot/Map.swift
@@ -12,6 +12,7 @@ public struct Map {
     public let id : String
     public let resourceState : ResourceState
     public let summaryPolyline : String?
+    public let polyline : String?
 }
 
 extension Map : Equatable {}
@@ -19,5 +20,6 @@ extension Map : Equatable {}
 public func ==(lhs : Map, rhs : Map) -> Bool {
     return  lhs.id              == rhs.id &&
             lhs.resourceState   == rhs.resourceState &&
-            lhs.summaryPolyline == rhs.summaryPolyline
+            lhs.summaryPolyline == rhs.summaryPolyline &&
+            lhs.polyline        == rhs.polyline
 }
