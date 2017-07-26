4e70fe0a60cbcbb0e46b4c4962b380879b573835
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-07-26 09:42:32 +0200

    Create request to upload a file

---
+++ StravaZpot/UploadAPI.swift
@@ -0,0 +1,30 @@
+//
+//  UploadAPI.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 24/7/17.
+//  Copyright © 2017 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public class UploadAPI {
+    private let client : HTTPClient
+    
+    public init(client : HTTPClient) {
+        self.client = client
+    }
+    
+    public func upload(file : URL,
+                       withFilename filename : String,
+                       withDataType dataType : DataType,
+                       withActivityType activityType : UploadActivityType? = nil,
+                       withName name : String? = nil,
+                       withDescription description : String? = nil,
+                       isPrivate : Bool = false,
+                       hasTrainer : Bool = false,
+                       isCommute : Bool = false,
+                       withExternalID externalID : String? = nil) -> UploadFileRequest {
+        return UploadFileRequest(client: self.client, file: file, filename: filename, dataType: dataType, activityType: activityType, name: name, description: description, isPrivate: isPrivate, hasTrainer: hasTrainer, isCommute: isCommute, externalID: externalID)
+    }
+}
