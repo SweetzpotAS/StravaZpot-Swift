3a470f597965c4d854a2a8b958d544a12f276148
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-07-26 09:58:49 +0200

    Create struct to represent app credentials

---
+++ StravaZpot/AppCredentials.swift
@@ -0,0 +1,14 @@
+//
+//  AppCredentials.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 26/7/17.
+//  Copyright © 2017 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct AppCredentials {
+    let cliendID : Int
+    let clientSecret : String
+}
