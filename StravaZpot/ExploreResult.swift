0b8f1ecffd783ab2f5cc26bdc5b605925bb478c7
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-07-27 13:15:56 +0200

    Create struct to represent explore result

---
+++ StravaZpot/ExploreResult.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreResult.swift
+//  StravaZpot
+//
+//  Created by Tomás Ruiz López on 27/7/17.
+//  Copyright © 2017 SweetZpot AS. All rights reserved.
+//
+
+import Foundation
+
+public struct ExploreResult {
+    let segments : EquatableArray<Segment>
+}
