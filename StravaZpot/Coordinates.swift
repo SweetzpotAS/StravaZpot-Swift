c805a96c869fc926178b2658d7bde4e8caeeaeaa
Frederik Riedel <frederik.riedel@frogg.io>
2018-12-04 12:41:35 +0100

    Added public initializer to Coordinates struct so it is accessible from the Xcode project.

--- StravaZpot/Coordinates.swift
+++ StravaZpot/Coordinates.swift
@@ -11,6 +11,10 @@ import Foundation
 public struct Coordinates {
     public let latitude : Double
     public let longitude : Double
+    public init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
 }
 
 extension Coordinates : Equatable {}
